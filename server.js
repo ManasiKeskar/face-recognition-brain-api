88f2db22a036227faa230cd82bf099b9c57afade
Manasi Keskar <ManasiKeskar@users.noreply.github.com>
2021-02-04 20:45:53 -0800

    added connection string for Knex for heroku

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ const db = knex({
     client: 'pg',
     connection: {
     //   host : '127.0.0.1',
-      host : 'postgresql-polished-37376',
-      user : 'postgres',
-      password : 'postgres',
-      database : 'smartbrain'
+    //   host : 'postgresql-polished-37376',
+    //   user : 'postgres',
+    //   password : 'postgres',
+    //   database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl : true
     }
 });
 
